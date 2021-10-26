dc607eadb557b78e54e81bf5f1b164e79c70a8d9
Sam <klioop2@gmail.com>
2021-10-26 16:36:49 +0900

    Add test target

---
+++ SearchMovieByDate/SearchMovieByDate/Controllers/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  SearchMovieByDate
+//
+//  Created by klioop on 2021/10/26.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var serachTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func didTapSearchButton(_ sender: UIButton) {
+        
+    }
+    
+
+}
---
+++ SearchMovieByDate/SearchMovieByDateTests/SearchViewControllerTest.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewControllerTest.swift
+//  SearchMovieByDateTests
+//
+//  Created by klioop on 2021/10/26.
+//
+
+import XCTest
+
+class SearchViewControllerTest: XCTestCase {
+    
+    
+
+
+}
