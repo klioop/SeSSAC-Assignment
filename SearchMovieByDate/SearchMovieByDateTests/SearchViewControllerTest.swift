3a20f7c651d7fb3a3f842db26c63bf160f36d394
Sam <klioop2@gmail.com>
2021-10-26 16:46:16 +0900

    can init vc

--- SearchMovieByDate/SearchMovieByDateTests/SearchViewControllerTest.swift
+++ SearchMovieByDate/SearchMovieByDateTests/SearchViewControllerTest.swift
@@ -6,9 +6,19 @@
 //
 
 import XCTest
+@testable import SearchMovieByDate
 
 class SearchViewControllerTest: XCTestCase {
     
+    func test_canInit() throws {
+        let bundle = Bundle(for: SearchViewController.self)
+        let sb = UIStoryboard(name: "Main", bundle: bundle)
+        
+        let initialVC = sb.instantiateInitialViewController()
+        let navVC = try XCTUnwrap(initialVC as? UINavigationController)
+        
+        _ = try XCTUnwrap(navVC.topViewController as? SearchViewController)
+    }
     
 
 
