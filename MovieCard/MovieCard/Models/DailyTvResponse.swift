8c9fd2fb409ebcebeb486c7d27f7ea060910ecca
Sam <klioop2@gmail.com>
2021-10-28 14:11:20 +0900

    daily tv request response model

---
+++ MovieCard/MovieCard/Models/DailyTvResponse.swift
@@ -0,0 +1,20 @@
+//
+//  DailyTvResponse.swift
+//  MovieCard
+//
+//  Created by klioop on 2021/10/28.
+//
+
+import Foundation
+
+struct DailyTvResponse {
+    let title: String
+    let rate: Double
+    let id: Int
+    let posterPath: String
+    let backDropImagePath: String
+    let genreIDs: [Int]
+    let firstAirDate: String
+    let overView: String
+}
+
