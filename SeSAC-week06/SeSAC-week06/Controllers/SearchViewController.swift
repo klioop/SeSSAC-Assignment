e129c5d250d511f937183fd80754e4d7ab101d8d
Sam <klioop2@gmail.com>
2021-11-01 22:13:42 +0900

    SearchViewController 파일 생성

---
+++ SeSAC-week06/SeSAC-week06/Controllers/SearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.swift
+//  SeSAC-week06
+//
+//  Created by klioop on 2021/11/01.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    static let sbID = "SearchViewController"
+    
+}
