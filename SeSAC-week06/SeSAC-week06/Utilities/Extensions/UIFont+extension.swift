008ce63a042aa4cfb06d640a6ba906f8a00509a1
Sam <klioop2@gmail.com>
2021-11-01 15:09:44 +0900

    폰트 자원 추가

--- SeSAC-week06/SeSAC-week06/Utilities/Extensions/UIFont+extension.swift
+++ SeSAC-week06/SeSAC-week06/Utilities/Extensions/UIFont+extension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+/*
+    S-CoreDream-1Thin
+    S-CoreDream-2ExtraLight
+    S-CoreDream-4Regular
+    S-CoreDream-3Light
+    S-CoreDream-5Medium
+    S-CoreDream-6Bold
+    S-CoreDream-7ExtraBold
+    S-CoreDream-8Heavy
+    S-CoreDream-9Black
+*/
