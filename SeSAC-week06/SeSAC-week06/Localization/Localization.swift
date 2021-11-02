15d43d0c773f5b588e14fafbbb63ea556a0d34a0
Sam <klioop2@gmail.com>
2021-11-02 10:38:12 +0900

    Install realm

---
+++ SeSAC-week06/SeSAC-week06/Localization/Localization.swift
@@ -0,0 +1,13 @@
+//
+//  Localization.swift
+//  SeSAC-week06
+//
+//  Created by klioop on 2021/11/02.
+//
+
+import Foundation
+
+enum AddViewLocalization: String {
+    case save = "save_button"
+    case cancel = "cancel_button"
+}
